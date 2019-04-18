21c4e1c8d55c3afbfbe5248cfd3e267754ff4c20
Bhabukpokharel <bhabukpokharel20@gmail.com>
2019-04-18 13:34:04 +0545

    First Commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from distutils.core import setup
+
+
+DESCRIPTION = "python package for accesign data from the BEA data API"
+
+PACKAGES =['pythonbea']
+
+
+VERSION= "0.0.1"
+
+setup( 
+	name ="pythonbea",
+	packages=PACKAGES,
+	description= DESCRIPTION,
+	author ='Bhabuk Pokharel',
+	author_email ="bhabukpokharel20@gmail.com"
+	)
+	
